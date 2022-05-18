bbe97aacee4a9417761d16f7fb9d0d18b0f0ac55
shiloganon <shilo1ganon@gmail.com>
2022-05-18 11:22:42 +0300

    Revert "First commit"
    This reverts commit 619c20f765ae7e17182ff3d69e32647b8b7495b8.

--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import authRoutes from './auth/authentication.routes';
-
-const router = express.Router();
-router.use('/api/', authRoutes);
-
-export default router;
